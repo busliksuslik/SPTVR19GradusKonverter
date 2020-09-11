a7188a1d45715fff98bf9c67d2166f9051c14cbf
Oleg <oleg.harlamov@ivkhk.ee>
2020-09-11 11:09:30 +0300

    Добавлена конвертация по кельвину, переработан вывод

--- src/sptvr19graduskonverter/App.java
+++ src/sptvr19graduskonverter/App.java
@@ -14,19 +14,25 @@ import java.util.Scanner;
 class App {
     private Scanner scan = new Scanner(System.in);
     public void Start(){
-        System.out.printf("incert operation%nFrom C to F - 1%nFrom F to C - 2");
+        System.out.printf("select temperature%nC - 1%nF - 2%nK - 3 ");
         int choise = scan.nextInt();
         switch(choise){
             case 1:{
                 System.out.print("incert temperature in celsius: ");
                 double number = scan.nextDouble();
-                System.out.printf("Ans = %.3f", number*1.8+32);
+                System.out.printf("F = %.3f\nK = %.2f", number*1.8+32 , number + 273.15);
                 break;
             }
             case 2:{
                 System.out.print("incert temperature in Fahrenheit: ");
                 double number = scan.nextDouble();
-                System.out.printf("Ans = %.3f", (number-32)/1.8);
+                System.out.printf("C = %.3f\nK = %.3f", (number-32)/1.8,(number-32)/1.8+273.15 );
+                break;
+            }
+            case 3:{
+                System.out.print("incert temperature in Kelvin: ");
+                double number = scan.nextDouble();
+                System.out.printf("C = %.3f\nF = %.3f", number - 273.15,(number-273.15)*1.8+32);
                 break;
             }
         }
